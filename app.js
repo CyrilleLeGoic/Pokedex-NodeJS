387426dc7757dd2decc06b2dc794383add3de27b
Cyrille <cyrillelegoic@gmail.com>
2023-01-21 08:34:16 +0100

    implementation CORS policy

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
-const sequelize = require('./src/db/sequelize');
+const sequelize = require('./src/db/sequelize');    
+const cors = require('cors')
 
 
 //* Déclaration de mes variables *//
@@ -16,6 +17,8 @@ const port = process.env.PORT || 3000;
 app
     .use(favicon(__dirname + '/favicon.ico'))
     .use(bodyParser.json())
+    .use(cors())
+
 
     sequelize.initDb()
 
